d349aa17185947e243c5b1db77c1277a46b48c1c
Manuel Gomes Rosmaninho <mgrosmaninho@hotmail.com>
2024-05-24 13:06:12 +0100

    hello-world

---
+++ hello.cob
@@ -0,0 +1,12 @@
+       IDENTIFICATION DIVISION.
+        PROGRAM-ID.  HELLO-WORLD.
+        AUTHOR. Manuel Gomes Rosmaninho.
+        DATE-WRITTEN. 24/05/24. 
+        DATE-COMPILED. 24/05/24.
+      *
+      * DESCRIPTION: THE HELLO-WORLD PROGRAM.
+      *
+       PROCEDURE DIVISION.
+       A001-MAIN.
+           DISPLAY "Hello, world!".
+           STOP RUN.

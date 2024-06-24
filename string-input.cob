89822f150d724e944187e033c5e68ad219dd42a8
Manuel Gomes Rosmaninho <mgrosmaninho@hotmail.com>
2024-06-24 16:57:13 +0100

    string-input with comments line by line

--- string-input.cob
+++ string-input.cob
@@ -1,17 +1,31 @@
-       IDENTIFICATION DIVISION. 
+       *> setup the identification division
+       IDENTIFICATION DIVISION.
+        *> setup the program id
         PROGRAM-ID. STRING-INPUT.
+        *> setup the author
         AUTHOR. Manuel Gomes Rosmaninho.
+        *> setup the date written
         DATE-WRITTEN. 24/05/24.
+        *> setup the date compiled
         DATE-COMPILED. 24/05/24.
       ****************************************************************
       * DESCRIPTION: TAKE STRING INPUT AND DISPLAY.
       ****************************************************************
+       *> setup the environment division 
        ENVIRONMENT DIVISION.
+       *> setup the data division
        DATA DIVISION.
+        *> working storage defines variables
         WORKING-STORAGE SECTION.
-       77 USER-INPUT  PIC A(20).
+       *> defines an alphabetic string
+       01 USER-INPUT  PIC A(20).
+       *> setup the procedure division
        PROCEDURE DIVISION.
-           DISPLAY "Input a text up to 20 characters". 
+           *> print a string
+           DISPLAY "Input a text up to 20 characters".
+           *> gets the user input
            ACCEPT USER-INPUT.
+           *> print the message and the user input
            DISPLAY "The user input is: " USER-INPUT.
+           *> stop the program
            STOP RUN.

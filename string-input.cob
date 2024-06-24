568179671956c8f336137844109b5fb4fa2a15bc
Manuel Gomes Rosmaninho <mgrosmaninho@hotmail.com>
2024-06-24 11:49:07 +0100

    string-input

--- string-input.cob
+++ string-input.cob
@@ -1,13 +1,16 @@
-       IDENTIFICATION DIVISION.
-        PROGRAM-ID.  HELLO-WORLD.
+       IDENTIFICATION DIVISION. 
+        PROGRAM-ID. STRING-INPUT.
         AUTHOR. Manuel Gomes Rosmaninho.
-        DATE-WRITTEN. 24/05/24. 
+        DATE-WRITTEN. 24/05/24.
         DATE-COMPILED. 24/05/24.
       ****************************************************************
-      * DESCRIPTION: THE HELLO-WORLD PROGRAM.
+      * DESCRIPTION: TAKE STRING INPUT AND DISPLAY.
       ****************************************************************
        ENVIRONMENT DIVISION.
        DATA DIVISION.
+        WORKING-STORAGE SECTION.
+       77 USER-INPUT  PIC A(20).
        PROCEDURE DIVISION.
-           DISPLAY "Hello, world!".
+           ACCEPT USER-INPUT.
+           DISPLAY "The user input is: " USER-INPUT.
            STOP RUN.

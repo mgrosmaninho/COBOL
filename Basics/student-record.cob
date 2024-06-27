e28ab5dc9451bb82d2f1f202c3e01374da5bd2e7
Manuel Gomes Rosmaninho <mgrosmaninho@hotmail.com>
2024-06-27 12:24:56 +0100

    Updated comments, inputs and outputs

--- Basics/student-record.cob
+++ Basics/student-record.cob
@@ -10,7 +10,7 @@
         DATE-COMPILED. 26/06/24.
       *********************************************************
       * DESCRIPTION: TAKES A STUDENT DETAILS AND DISPLAY
-      * DETAILS: NAME, SURNAME, GENDER, COURSE
+      * DETAILS: ID, NAME, SURNAME, GENDER, COURSE
       *********************************************************
        *> setup the environment division
        ENVIRONMENT DIVISION.
@@ -19,19 +19,35 @@
        *> working storage defines variables
        WORKING-STORAGE SECTION.
        01 STUDENT-DETAILS.
-          02 STUDENT-ID        PIC 9(6).
-          02 STUDENT-FULLNAME.
-             03 FORENAME       PIC X(10).
-             03 SURNAME        PIC X(10).
-          02 GENDER            PIC X.
-          02 COURSE-CODE       PIC X(4).
+          05 STUDENT-ID        PIC 9(6).
+          05 STUDENT-FULLNAME.
+             10 FORENAME       PIC X(10).
+             10 SURNAME        PIC X(10).
+          05 GENDER            PIC X.
+          05 COURSE-CODE       PIC X(4).
        *> setup the procedure division
        PROCEDURE DIVISION.
-           *> print a string
+           *> print a string and get the student ID
            DISPLAY "Input student id: " WITH NO ADVANCING.
            ACCEPT STUDENT-ID.
-           DISPLAY "Input student name: " WITH NO ADVANCING.
-           ACCEPT STUDENT-FULLNAME.
+           *> print a string and get the student forename
+           DISPLAY "Input student forename: " WITH NO ADVANCING.
+           ACCEPT FORENAME.
+           *> print a string and get the student surname
+           DISPLAY "Input student surname: " WITH NO ADVANCING.
+           ACCEPT SURNAME.
+           *> print a string and get the student gender
+           DISPLAY "Input student gender: " WITH NO ADVANCING.
+           ACCEPT GENDER.
+           *> print a string and get the student course
+           DISPLAY "Input student couse: " WITH NO ADVANCING.
+           ACCEPT COURSE-CODE.
+           *> print student ID
            DISPLAY "Student ID: " STUDENT-ID.
-           DISPLAY "Student fullname is: " FORENAME.
+           *> print student full name
+           DISPLAY "Student fullname is: " STUDENT-FULLNAME.
+           *> print student gender
+           DISPLAY "Student gender is: " GENDER.
+           *> print student course
+           DISPLAY "Student course is: " COURSE-CODE.
            STOP RUN.

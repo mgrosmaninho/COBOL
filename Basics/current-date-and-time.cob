a9075f8136658000cf413e110a5ed3a7aeb3e09e
Manuel Gomes Rosmaninho <mgrosmaninho@hotmail.com>
2024-10-04 09:34:50 +0100

    improved version of the current date and time

--- Basics/current-date-and-time.cob
+++ Basics/current-date-and-time.cob
@@ -1,47 +1,39 @@
-       *> setup the identification division
-       IDENTIFICATION DIVISION.
-        *> setup the program id
-        PROGRAM-ID. CURRENT-DATE-AND-TIME.
-        *> setup the author
-        AUTHOR. Manuel Gomes Rosmaninho
-        *> setup the date written
-        DATE-WRITTEN. 26/06/24.
-        *> setup the date compiled
-        DATE-COMPILED. 26/06/24.
+       IDENTIFICATION DIVISION. *> Section for the program’s identification.
+        PROGRAM-ID. CURRENT-DATE-AND-TIME. *> Program name.
+        AUTHOR. Manuel Gomes Rosmaninho *> Author name.
+        DATE-WRITTEN. 26/06/24. *> Date the program was written.
+        DATE-COMPILED. 26/06/24. *> Date the program was compiled.
+
       *******************************************************
-      * DESCRIPTION: GET CURRENT DATE AND TIME AND DISPLAY
-      *******************************************************
-       *> setup the environment division
-       ENVIRONMENT DIVISION.
-       *> setup the data division
-       DATA DIVISION.
-       *> working storage defines variables
-       WORKING-STORAGE SECTION.
-       *> defines the current date
+      * DESCRIPTION: PROGRAM TO GET CURRENT DATE AND TIME, THEN DISPLAY THEM.
+      *******************************************************  
+      
+       ENVIRONMENT DIVISION. *> Section for the program’s environment.
+
+       DATA DIVISION. *> Section for the program’s data.
+       WORKING-STORAGE SECTION. *> Section for the program’s working storage.
+       *> Define a variable for the current date composed of year, month, day.
        01 CURRENT-DATE.
-          02 CURRENT-YEAR    PIC 9999.
-          02 CURRENT-MONTH   PIC 99.
-          02 CURRENT-DAY     PIC 99.
-       *> defines the current time
+          02 CURRENT-YEAR    PIC 9999. *> 4 digits for the year (YYYY).
+          02 CURRENT-MONTH   PIC 99.   *> 2 digits for the month (MM).
+          02 CURRENT-DAY     PIC 99.   *> 2 digits for the day (DD).
+       *> Define a variable for the current time composed of hour and minute.
        01 CURRENT-TIME.
-          02 CURRENT-HOUR    PIC 99.
-          02 CURRENT-MINUTE  PIC 99.
-       *> setup the procedure division
-       PROCEDURE DIVISION.
-           *> gets the date from the system (year, month and day)
-           *> and store in the variables using the format YYYYMMDD
+          02 CURRENT-HOUR    PIC 99.   *> 2 digits for the hour (HH).
+          02 CURRENT-MINUTE  PIC 99.   *> 2 digits for the minute (MM).
+       
+       PROCEDURE DIVISION. *> Section for the program’s procedure.
+           *> Get the current date from the system in the format YYYYMMDD.
            ACCEPT CURRENT-DATE FROM DATE YYYYMMDD.
-           *> gets the time form the system (hour and minute)
-           *> and store in the variable using the format HHMM
+           *> Get the current time from the system in the format HHMM.
            ACCEPT CURRENT-TIME FROM TIME.
-           *> print the current date in the format DD/MM/YYYY
+           *> Display the current date in the format DD/MM/YYYY
            DISPLAY "The date is "
                    CURRENT-DAY
                    "/"
                    CURRENT-MONTH
                    "/"
                    CURRENT-YEAR.
-           *> print the time in the format HH:MM
+           *> Display the current time in the format HH:MM.
            DISPLAY "The time is " CURRENT-HOUR ":" CURRENT-MINUTE.
-           *> stop the program
-           STOP RUN.
+           STOP RUN. *> End program execution.
